b5092a5702b43b6a1f4ac7438663be9d1b02ce0a
Olena40 <o.redchuk@gmx.de>
2021-12-08 17:04:29 +0100

    fifth

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -10,3 +10,20 @@ export const getAll = async (req, res) => {
   const types = await Type.findAll();
   return res.json(types);
 };
+
+export const destroy = async (req, res) => {
+  const { name } = req.body;
+  const type = await Type.destroy({ where: { name: value } });
+  return res.json({ message: "Deleted successfully" });
+};
+
+/* export const destroy = async (req,res) => { 
+  const { name } = req.params;   
+  Type.destroy({
+      where: {
+          name: req.params.name
+      }
+  })
+  res.status(200).json({message:"Deleted successfully"});
+ 
+  */
--- routes/typeRouter.js
+++ routes/typeRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { create, getAll } from "../controllers/typeController.js";
+import { create, getAll, destroy } from "../controllers/typeController.js";
 import { checkRole } from "../middleware/checkRoleMiddleware.js";
 
 const router = express.Router();
 
-// router.post("/", create);
-router.post("/", checkRole("ADMIN"), create);
+router.post("/", create);
+// router.post("/", checkRole("ADMIN"), create);
 router.get("/", getAll);
+router.delete("/", destroy);
 
 export default router;
